7d15ff8a976ddcf85b6f0a17162f976eb388a50f
Johnazoo <1010138814@qq.com>
2015-09-26 12:10:30 +0800

    页面切换效果实现

--- app/src/main/java/smallzoo/com/shengchen/DocumentActivity.java
+++ app/src/main/java/smallzoo/com/shengchen/DocumentActivity.java
@@ -1,19 +1,19 @@
 package smallzoo.com.shengchen;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class DocumentActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_document);
     }
+
     public void click(View view){
-        startActivity(new Intent(MainActivity.this,DocumentActivity.class));
-        overridePendingTransition(R.anim.scale_in, R.anim.scale_out);
+        finish();
     }
 }
